1cc92d42a6b14afca4cb3f294a4cda4e3601413b
Soham Kumar Dey <93932583+SohamDey80@users.noreply.github.com>
2025-11-28 11:50:42 +0530

    Add files via upload

---
+++ Looping/Upto10PrintAll.java
@@ -0,0 +1,10 @@
+package Looping;
+import java.util.*;
+
+public class Upto10PrintAll {
+    public static void main(String[] args){
+        for(int i = 0; i < 11; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
